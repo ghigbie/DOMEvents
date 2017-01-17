6d90732399ad9695912db5cb26c3f448141b56a6
George Higbie <gehigbie@gmail.com>
2017-01-17 14:58:07 -0700

    added links to stylesheet and javaSCript

--- jsDomEvents.js
+++ jsDomEvents.js
@@ -2,7 +2,14 @@
 
 var button = document.querySelector("button");
 var paragraph = document.querySelector("p");
+
+
 button.addEventListener("click", function(){
 	button.textContent = "Someone has clicked me!"
 	paragraph.textContent = "Someone as clicked the button!"
+
+	// button.addEventListener("click", function(){
+	// 	button.textContent = "Someone has clicked me twice!"
+	// 	paragraph.textContent = "Someone as clicked the button twice!"
+	// });
 });
