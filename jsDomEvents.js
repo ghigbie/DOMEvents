f46c25a9fece65a50fe727d8a09b32e5ab55a2dc
George Higbie <gehigbie@gmail.com>
2017-01-17 14:47:32 -0700

    added on Event Listeners to a button

--- jsDomEvents.js
+++ jsDomEvents.js
@@ -1,1 +1,8 @@
 //created in terminal
+
+var button = document.querySelector("button");
+var paragraph = document.querySelector("p");
+button.addEventListener("click", function(){
+	button.textContent = "Someone has clicked me!"
+	paragraph.textContent = "Someone as clicked the button!"
+});
